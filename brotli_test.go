501ebdf04044111dab9a1680e40f37fff4803201
Mike Houston <m@hou.st>
2015-10-27 23:32:59 +0000

    Added roundtrip compression tests from upstream repository to test compression levels and different input types.

--- brotli_test.go
+++ brotli_test.go
@@ -2,19 +2,20 @@ package brotli
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 
 	"github.com/kothar/brotli-go/dec"
 	"github.com/kothar/brotli-go/enc"
 )
 
-func TestRoundtrip(T *testing.T) {
+func TestSimpleString(T *testing.T) {
 	T.Log("Compressing test string")
 	s := []byte("Hello Hello Hello, Hello Hello Hello")
 	T.Logf("Original: %s\n", s)
 
 	params := enc.NewBrotliParams()
-	buffer1 := make([]byte, len(s)+100)
+	buffer1 := make([]byte, len(s)*2)
 	encoded, cerr := enc.CompressBuffer(params, s, buffer1)
 	if cerr != nil {
 		T.Error(cerr)
@@ -32,5 +33,60 @@ func TestRoundtrip(T *testing.T) {
 		T.Error("Decoded output does not match original input")
 	} else {
 		T.Log("Decoded output matches original input")
+	}
+}
+
+// Run roundtrip tests from Brotli repository
+func TestRoundtrip(T *testing.T) {
+	inputs := []string{
+		"testdata/alice29.txt",
+		"testdata/asyoulik.txt",
+		"testdata/lcet10.txt",
+		"testdata/plrabn12.txt",
+		"enc/encode.cc",
+		"shared/dictionary.h",
+		"dec/decode.c",
+	}
+
+	for _, file := range inputs {
+
+		input, err := ioutil.ReadFile(file)
+		if err != nil {
+			T.Error(err)
+		}
+
+		for _, quality := range []int{1, 6, 9, 11} {
+			T.Logf("Roundtrip testing %s at quality %d", file, quality)
+
+			params := enc.NewBrotliParams()
+			params.SetQuality(quality)
+
+			bro, err := enc.CompressBuffer(params, input, nil)
+			if err != nil {
+				T.Error(err)
+			}
+			T.Logf("  Compressed from %d to %d bytes, %.1f%%", len(input), len(bro), (float32(len(bro))/float32(len(input)))*100)
+
+			unbro, err := dec.DecompressBuffer(bro, nil)
+			if err != nil {
+				T.Error(err)
+			}
+
+			if len(input) != len(unbro) {
+				T.Errorf("Length of decompressed output (%d) doesn't match input (%d)", len(unbro), len(input))
+			}
+
+			if !bytes.Equal(input, unbro) {
+				T.Error("  Input does not match decompressed output")
+			}
+		}
+	}
+}
+
+func cap(s []byte, length int) []byte {
+	if len(s) > length {
+		return s[:length]
+	} else {
+		return s
 	}
 }
