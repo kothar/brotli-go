2c6ec0bde2cb58ad0bbbe5f656cc678db12caae6
Amos Wenger <fasterthanlime@gmail.com>
2015-11-17 15:33:25 +0100

    Better streaming encode test

--- enc/encode_stream_test.go
+++ enc/encode_stream_test.go
@@ -2,6 +2,7 @@ package enc
 
 import (
 	"bytes"
+	"log"
 	"strings"
 	"testing"
 )
@@ -9,29 +10,39 @@ import (
 func TestStreamEncode(T *testing.T) {
 	input1 := []byte(strings.Repeat("The quick brown fox jumps over the lazy dog", 100000))
 	inputSize := int64(len(input1))
+	log.Printf("inputSize=%d\n", inputSize)
+	params := NewBrotliParams()
 
-	compressor := NewBrotliCompressor(nil)
-	blockSize := compressor.GetInputBlockSize()
+	for lgwin := 16; lgwin <= 22; lgwin += 1 {
+		params.SetLgwin(lgwin)
+		compressor := NewBrotliCompressor(params)
+		blockSize := compressor.GetInputBlockSize()
 
-	fullOutput := make([]byte, 0)
+		fullOutput := make([]byte, 0)
 
-	pos := int64(0)
-	for pos < inputSize {
-		copySize := blockSize
-		remaining := inputSize - pos
-		if remaining < copySize {
-			copySize = remaining
+		rounds := 0
+		pos := int64(0)
+		for pos < inputSize {
+			rounds++
+			copySize := blockSize
+			remaining := inputSize - pos
+			if remaining < copySize {
+				copySize = remaining
+			}
+			compressor.CopyInputToRingBuffer(input1[pos : pos+copySize])
+			pos += copySize
+
+			output := compressor.WriteBrotliData(pos >= inputSize, false)
+			fullOutput = append(fullOutput, output...)
 		}
-		compressor.CopyInputToRingBuffer(input1[pos : pos+copySize])
-		pos += copySize
 
-		output := compressor.WriteBrotliData(pos >= inputSize, false)
-		fullOutput = append(fullOutput, output...)
-	}
+		fullOutput2, _ := CompressBuffer(params, input1, make([]byte, 0))
 
-	fullOutput2, _ := CompressBuffer(nil, input1, make([]byte, 0))
+		if !bytes.Equal(fullOutput, fullOutput2) {
+			T.Fatal("for lgwin %d, stream compression didn't give same result as buffer compression", params.Lgwin())
+		}
 
-	if !bytes.Equal(fullOutput, fullOutput2) {
-		T.Fatal("Stream compression didn't give same result as buffer compression")
+		outputSize := len(fullOutput)
+		log.Printf("lgwin=%d, rounds=%d, output=%d (%.4f%% of input size)\n", params.Lgwin(), rounds, outputSize, float32(outputSize)*100.0/float32(inputSize))
 	}
 }
