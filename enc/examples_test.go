1a5b3e3bc3fb147cdddee2e1d841f50115d6684f
Mike Houston <mike@kothar.net>
2017-05-20 10:48:31 +0100

    Update Travis configuration (#37)
    * Use GVM to download go versions
    * Fix lint warning

--- enc/examples_test.go
+++ enc/examples_test.go
@@ -5,14 +5,15 @@ import (
 	"os"
 )
 
-func ExampleBrotliParams(input []byte) []byte {
+func ExampleBrotliParams() {
+	var input []byte
 	params := NewBrotliParams()
 
 	// brotli supports quality values from 0 to 11 included
 	// 0 is the fastest, 11 is the most compressed but slowest
 	params.SetQuality(0)
 	compressed, _ := CompressBuffer(params, input, make([]byte, 0))
-	return compressed
+	_ = compressed
 }
 
 func ExampleBrotliWriter() {
